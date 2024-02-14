df93de017f5dcf4b8c7747d2464503576f6890f6
Talwinder87 <158572254+Talwinder87@users.noreply.github.com>
2024-02-14 18:11:14 +0530

    responsive site using bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,311 @@
+	<!doctype html>
+	<html lang="en">
+	  <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bootstrap demo</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+		<link rel="stylesheet" href="bootstrap.css">
+	 </head>
+	  <body>
+	  <div class="hero-section pb-5">
+	  <div class="container-fluid">
+			  <div class="contact-nav">
+				 <a href="" class="text-decoration-none"><i class="bi bi-telephone-fill" style="color:#ff8a1d"></i><span style="color:white">Call : +01 123455678990 </span></a>
+				<a href="" class="text-decoration-none"><i class="bi bi-envelope-fill" style="color:#ff8a1d"></i><span style="color:white">Email : demo@gmail.com</span></a>
+				</div>
+		</div>
+		
+		<nav class="navbar navbar-expand-lg">
+	  <div class="container-fluid">
+		<a class="navbar-brand" href="#">INANCE</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+		  <ul class="navbar-nav " >
+			<li class="nav-item pe-5">
+			  <a class="nav-link active " aria-current="page" href="#">Home</a>
+			</li>
+			<li class="nav-item pe-5">
+			  <a class="nav-link "  href="#">About</a>
+			</li>
+			<li class="nav-item pe-5">
+			  <a class="nav-link"  href="#">Services</a>
+			</li>
+			<li class="nav-item pe-5">
+			  <a class="nav-link" href="#">Contact us</a>
+			</li>
+			
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+	<div class="container">
+	<div class="row">
+
+		<div class="col-md-6 " >
+		  <h1 id="details" >Repair and<br>
+				Maintenance<br>
+				Services</h1>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui harum voluptatem adipisci. Quos molestiae saepe dicta nobis pariatur, tempora iusto, ad possimus soluta hic praesentium mollitia consequatur beatae, aspernatur culpa.</p>
+				<a href="#" class="contact">CONTACT US</a>
+		</div>
+		<div class="col-md-6 d-none d-md-block">
+		  <img src="images/slider-img.png" class="img-fluid" id="pic">
+		</div>
+	</div>	
+
+
+	</div>	
+	</div>
+	
+	<!-- feauters section-->
+	<div class="container-fluid " id="cont">
+	<div class="row justify-content-center">
+		<div class="col-md-3 py-5 py-lg-0">
+			<div class="box">
+			
+				<div class="img-box">
+				<i class="bi bi-tools" id="tools"></i>
+				</div>
+				<br>
+				<h5 class="colr">REPAIR</h5>
+			</div>
+		
+		</div>
+		<div class="col-md-3 py-5 py-lg-0">
+			<div class="box">
+				<div class="img-box">
+				<i class="bi bi-house-door-fill" id="size"></i>
+				</div>
+				<br>
+				<h5 class="colr">IMPROVE</h5>
+			
+		
+			</div>
+		</div>
+		<div class="col-md-3 py-5 py-lg-0">
+			<div class="box">
+			<div class="img-box">
+				<i class="bi bi-house-gear-fill" id="tools2"></i>
+				</div>
+				<br>
+				<h5 class="colr">MAITAIN</h5>
+			</div>
+		</div>
+	</div>
+	</div>
+	<!--features sction end-->
+	
+	<!-- about section start-->
+	<div class="abt-sec">
+	<div class="container mt-5" id="mrgn-top">
+	
+		<div class="row justify-content-center">
+		
+			<div class="col-lg-5 col-10 my-3" id="m">
+				<h3>About Us </h3>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomisedThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised </p>
+				<a href="" class="anchr">Read More </a>
+			</div>
+			<div class="col-lg-7 col-10 my-3">
+				<img  src="images/about-img.jpg" class="img-fluid">
+			</div>
+		</div>
+	
+	</div>
+	</div>
+	<!-- Profesional section-->
+	<div class="prf-sec">
+	<div class="container" id="prf">
+		<div class="row justify-content-center">
+			<div class="col-lg-6 col-10 my-3 ">
+			<img src="images/professional-img.png" class="img-fluid">
+			
+			</div>
+			
+			<div class="col-lg-6 col-10 my-4" id="prf-p">
+			<h2 id="headr">
+				We Provide Professional
+							<br>Home Services. </h2>
+				<p>randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All randomised words which don't look even slightly </p>
+				
+				<a href="" class="prf-a">Read More</a>
+			</div>
+		</div>
+
+	</div>
+	</div>
+	<!--professinal end-->
+	
+	<!--service sec start-->
+	<div class="service">
+		<div class="container">
+		
+			<div class="row">
+				<div class="col-12 py-5">
+					<h2 class="text-center">OUR SERVICES</h2>
+				</div>	
+				<div class="col-md-4 ">	
+					<div class="box">
+						<div class="img-box">
+							<i class="bi bi-tools" id="tool"></i>
+						</div>
+						
+						<div class="details-box">
+						<h5 class="text-center">Maintenance</h5>
+						<p class="px-3">when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal </p>
+						</div>
+					</div>
+				
+				</div>
+				
+				<div class="col-md-4 py-2">	
+				
+				<div class="box">
+						<div class="img-box">
+							<i class="bi bi-plugin" id="tool2"></i>
+						</div>
+						
+						<div class="details-box">
+						<h5 class="text-center">Maintenance</h5>
+						<p class="px-3">when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal </p>
+						</div>
+					</div>
+				
+				</div>
+				
+				<div class="col-md-4 py-2">	
+				<div class="box">
+						<div class="img-box">
+							<i class="bi bi-tools" id="tool"></i>
+						</div>
+						
+						<div class="details-box">
+						<h5 class="text-center">Maintenance</h5>
+						<p class="px-3">when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal </p>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 py-5 text-center">
+					<a href="" class="srvc-btn">View more</a>
+				</div>	
+			</div>
+		</div>
+	</div>
+	<!--Service sec end-->
+	
+	<!--CONTACT US -->
+	<div class="sec-contact">
+	
+			<div class="container">
+				<div class="row justify-content-around">
+					<div class="col-lg-5">
+					
+						<h3>Contact Us</h3><br>
+						<div class="input-group mb-3">
+							  
+							  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+							</div>
+							
+							<div class="input-group mb-3">
+							 <input type="email" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
+							</div>
+							
+							<div class="input-group mb-3">
+							 <input type="text" class="form-control" placeholder="Phone Number" aria-label="Username" aria-describedby="basic-addon1">
+							</div>
+
+							
+
+
+							<div class="input-group">
+							  
+							  <textarea class="form-control" aria-label="Message"></textarea>
+							</div>
+							<div class="col-12 pt-3">
+							<button class="btn btn-primary" type="submit">Send</button>
+						  </div>
+
+					</div>
+					
+					<div class="col-lg-5 ps-4">
+					<div class="embed-responsive">
+					<iframe   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d54888.76953649845!2d76.14299190196523!3d30.70298845703818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1707034351266!5m2!1sen!2sin" allowfullscreen style="border:0;"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					</div>
+					
+					</div>
+				
+				</div>
+			</div>
+	</div>
+	
+	<!--contact sec end-->
+	
+	<!--Info-sec-->
+		<div class="info mt-3 pb-5">
+			<div class="container ">
+				<h3 class="text-center pt-5">Get In Touch</h3>
+				<div class="row mt-5">
+					<div class="col-md-4">
+						<div class="item">
+						<a href=""><div class="img-bx">
+						<i class="bi bi-geo-alt"></i>
+						</div></a>
+						<p class="pt-3">lorem simple dummy text  </p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="item">
+						<a href=""><div class="img-bx">
+						<i class="bi bi-telephone"></i>
+						</div></a>
+						<p class="pt-3">+9924345566  </p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="item">
+						<a href=""><div class="img-bx">
+						<i class="bi bi-envelope"></i>
+						</div></a>
+						<p class="pt-3">demo@gmail.com  </p>
+						</div>
+					</div>
+					
+					
+				</div>
+				<div class="row justify-content-center">
+						<div class="col-lg-5">
+						<h4 class="text-center">Follow Us</h4>
+						<div class="follow bg-white text-center">
+						<a href=""><i class="bi bi-facebook"></i></a>
+						<a href=""><i class="bi bi-twitter"></i></a>
+						<a href=""><i class="bi bi-instagram"></i></a>
+						<a href=""><i class="bi bi-youtube"></i></a>
+						</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	<!--footer-->
+	
+		<div class="footer py-4">
+			<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-8 border-top border-white">
+				<p class="text-center pt-3">© 2024 All Rights Reserved By Free Html Templates </p>
+				</div>
+			
+			</div>
+		</div>
+		</div>
+	
+		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	  </body>
+	</html>
